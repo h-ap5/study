b9c51856b43f719e5772d5b077c86175f557dc2d
h-ap5 <h4173000@gmail.com>
2026-09-19 18:04:54 +0900

    Update muse.user.js

--- muse.user.js
+++ muse.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ✨ Crack Muse Writer (AI 답변 커스텀)
 // @namespace    muse writer
-// @version      5.2.17
+// @version      5.2.18
 // @description  Crack 캐릭터챗 입력을 맥락·프로필·유저 노트·참고자료·서사 나침반에 맞춰 다듬고, 단기·장기 기억과 최신 에리 로어를 읽기 전용으로 참고하며 유저 입력 번역까지 처리하는 AI 집필 보조 도구
 // @updateURL    https://github.com/h-ap5/study/raw/refs/heads/main/muse.user.js
 // @downloadURL  https://github.com/h-ap5/study/raw/refs/heads/main/muse.user.js
